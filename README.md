92339af5ef168b36068028dad89f1d5658a14f55
Annapurna Agrawal <smartherd@gmail.com>
2022-03-20 19:05:05 +0530

    Chapter 2 - Built-in data types

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# TypeScript-Fudamentals
-Learn TypeScript by creating a Game. TODO
+File name: main01.txt
+Topics demostrated:
+    - Declaring basic typescript function with type annotations
+    - Default parameter and optional parameter
+    - adding functions to demo app
+    - arrow function
+
+
+
+File name: main.ts
+- final file after the chapter ends
+- ussing arrow functions as function types
--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,16 @@
 // Function executes when Start Game button is clicked
 function startGame() {
+
+    let playerName: string = "Annanya";
+
+    let welcomeMessage = welcomePlayer(playerName);
+    
     var messageElement = document.getElementById('message');
-    messageElement!.innerText  = 'Welcome! Starting a new game..';
+    messageElement!.innerText  = welcomeMessage;
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
+
+function welcomePlayer(name) {
+    return `Hello ${name}! Let's start a new game..`;
+}
